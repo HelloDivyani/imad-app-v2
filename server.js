f1c6bab6e6a51b8d79da985ef3c2f6914c29aca7
Divyani Mittal <hellodivyani@gmail.com>
2017-02-10 20:21:39 +0530

    Added more urls in server

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+
+app.get('/article-one',function(req,res){
+	res.send('The article one requested');
+});
+app.get('article-two',function(req,res){
+	res.send('The article two requested');
+});
+app.get('article-three',function(req,res){
+	res.send('The article three is requested');
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
