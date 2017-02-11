7cdf2eb8ba58fefcbf7585e165898116bcaeedbb
Divyani Mittal <hellodivyani@gmail.com>
2017-02-11 11:32:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,9 +8,9 @@ var img=document.getElementById("madi");
 var marginLeft=0;
 function moveRight()
 {
-    marginleft=marginLeft+1;
+    marginleft=marginLeft+4;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick = function(){
-    var interval=setInterval(moveRight,50);
+    var interval=setInterval(moveRight,100);
 };
