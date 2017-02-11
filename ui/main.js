743466b14b46b42b4d14fececaaace95714b53f4
Divyani Mittal <hellodivyani@gmail.com>
2017-02-11 11:33:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="New Value ";
 // Make the image move
-
 var img=document.getElementById("madi");
-var marginLeft=0;
-function moveRight()
-{
-    marginleft=marginLeft+4;
-    img.style.marginLeft=marginLeft+'px';
-}
 img.onclick = function(){
-    var interval=setInterval(moveRight,100);
+    img.style.marginLeft='100px';
 };
