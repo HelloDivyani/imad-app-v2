75464d2ce51d84c32af0afd986b90e3c0c946215
Divyani Mittal <hellodivyani@gmail.com>
2017-02-11 14:03:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ img.onclick = function(){
     var interval=setInterval(moveRight,50);
 };**/
 
-var counter;
+var counter=0;
 var button=document.getElementById('counter');
 button.onclick=function(){
   
