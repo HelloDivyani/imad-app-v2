c0328ce1cfaa8e3ab71c86f32d91bbd08ee4b767
Divyani Mittal <hellodivyani@gmail.com>
2017-02-11 16:09:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,13 +14,36 @@ img.onclick = function(){
    
     var interval=setInterval(moveRight,50);
 };**/
-
 var counter=0;
 var button=document.getElementById('counter');
 button.onclick=function(){
-  
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter.toString();
+    //Create a quest object
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange  = function()
+    {
+      // Check 
+      if(request.readyState === XMLHttpRequest.DONE)
+      {
+          if(request.status === 200)
+          {
+              // do some action
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+              
+              
+          }
+      }
+    };
+    
+    
+    request.open('GET','http://hellodivyani.imad.hasura-app.io/counter',true);
+    res.send(null);
+    
+    
+    
+    
+   
   
 };
